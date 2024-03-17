405e51ec28966579a16d2d3688d39846330ea9a5
Sangeetha <manojimala1965@gmail.com>
2024-03-17 18:11:46 +0530

    Code to perform additional functionality

---
+++ ExtendedCalculator.java
@@ -0,0 +1,16 @@
+class ExtendedCalculator{
+   
+    float square(float operand_1){
+        return(operand_1*operand_1);
+    }
+
+     
+    float cube(float operand_1){
+        return(operand_1*operand_1*operand_1);
+    }
+
+    
+    float modulo(float operand_1,float operand_2){
+    return(operand_1 % operand_2);
+     }
+}
